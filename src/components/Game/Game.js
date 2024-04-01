0665354cde3437421f488e1760adb64878c7db69
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-04-01 11:54:32 +0530

    Game Logic - Input, Grid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample, range } from "../../utils";
+import { WORDS } from "../../data";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guessList, setGuessList] = useState([]);
+
+  return (
+    <>
+      <GuessOutput guessList={guessList} />
+      <Guess setGuessList={setGuessList} />
+    </>
+  );
+}
+
+function GuessOutput({ guessList }) {
+  return (
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <GuessCell key={num} value={guessList[num]} />
+      ))}
+    </div>
+  );
+}
+
+function GuessCell({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((num) => (
+        <span className="cell" key={num}>
+          {value ? value[num] : undefined}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+function Guess({ setGuessList }) {
+  const [guess, setGuess] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    setGuessList((items) => [...items, guess]);
+    setGuess("");
+  }
+
+  return (
+    <>
+      <form className="guess-input-wrapper" onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          required
+          minLength={5}
+          maxLength={5}
+          id="guess-input"
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
+  );
 }
 
 export default Game;
